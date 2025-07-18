b6df180a4cbbcc5b133718d4ae1f30da44564072
SamraAzizi <samarraazizi@gmail.com>
2025-07-18 14:08:54 +0500

    basic pytorch

--- tutorials/basics/feedforward-neural-network/main.py
+++ tutorials/basics/feedforward-neural-network/main.py
@@ -13,3 +13,13 @@ num_classes = 10
 num_epochs = 5
 batch_size = 100
 learning_rate = 0.001
+
+# MNIST dataset 
+train_dataset = torchvision.datasets.MNIST(root='../../data', 
+                                           train=True, 
+                                           transform=transforms.ToTensor(),  
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='../../data', 
+                                          train=False, 
+                                          transform=transforms.ToTensor())
