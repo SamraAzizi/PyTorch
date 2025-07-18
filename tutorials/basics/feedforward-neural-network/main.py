bdcfacac2709d4f051e894c792c094320780ef9a
SamraAzizi <samarraazizi@gmail.com>
2025-07-18 14:08:33 +0500

    basic pytorch

--- tutorials/basics/feedforward-neural-network/main.py
+++ tutorials/basics/feedforward-neural-network/main.py
@@ -6,3 +6,10 @@ import torchvision.transforms as transforms
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# Hyper-parameters 
+input_size = 784
+hidden_size = 500
+num_classes = 10
+num_epochs = 5
+batch_size = 100
+learning_rate = 0.001
