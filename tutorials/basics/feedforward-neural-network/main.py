5a4b6e70ef44e4a6bc3c619627adbb1a647d59e6
SamraAzizi <samarraazizi@gmail.com>
2025-07-19 22:06:59 +0500

    basic pytorch

--- tutorials/basics/feedforward-neural-network/main.py
+++ tutorials/basics/feedforward-neural-network/main.py
@@ -63,3 +63,17 @@ for epoch in range(num_epochs):
         # Move tensors to the configured device
         images = images.reshape(-1, 28*28).to(device)
         labels = labels.to(device)
+
+
+         # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if (i+1) % 100 == 0:
+            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
+                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
