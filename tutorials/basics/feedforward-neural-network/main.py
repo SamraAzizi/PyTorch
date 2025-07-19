0aea58e9ddadfdd55e0ebd81e5a87bb97115ea84
SamraAzizi <samarraazizi@gmail.com>
2025-07-19 22:05:38 +0500

    basic pytorch

--- tutorials/basics/feedforward-neural-network/main.py
+++ tutorials/basics/feedforward-neural-network/main.py
@@ -42,3 +42,11 @@ class NeuralNet(nn.Module):
         self.fc1 = nn.Linear(input_size, hidden_size) 
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size, num_classes)  
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+
+model = NeuralNet(input_size, hidden_size, num_classes).to(device)
