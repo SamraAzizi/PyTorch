89206faf92f1fa7d6fd47fe31888844e62d17b84
SamraAzizi <samarraazizi@gmail.com>
2025-07-19 22:05:11 +0500

    basic pytorch

--- tutorials/basics/feedforward-neural-network/main.py
+++ tutorials/basics/feedforward-neural-network/main.py
@@ -33,3 +33,12 @@ train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
                                           batch_size=batch_size, 
                                           shuffle=False)
+
+
+# Fully connected neural network with one hidden layer
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(NeuralNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size) 
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)  
