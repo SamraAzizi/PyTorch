9e004c61ba8e4b93c957ac6ed9324b9657e27fe2
SamraAzizi <samarraazizi@gmail.com>
2025-07-20 16:44:59 +0500

    basic pytorch

---
+++ tutorials/basics/linear-regression/main.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Hyper-parameters
+input_size = 1
+output_size = 1
+num_epochs = 60
+learning_rate = 0.001
