3de3b2708c239eec93501a90dd75b41c6dea7ff6
SamraAzizi <samarraazizi@gmail.com>
2025-07-21 12:19:38 +0500

    basic pytorch

---
+++ tutorials/basics/logistic-regression/main.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+
+# Hyper-parameters 
+input_size = 28 * 28    # 784
+num_classes = 10
+num_epochs = 5
+batch_size = 100
+learning_rate = 0.001
