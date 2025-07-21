b62edb6b7c04101d90a12c54e6cb1e395e4af1a6
SamraAzizi <samarraazizi@gmail.com>
2025-07-21 12:19:56 +0500

    basic pytorch

--- tutorials/basics/logistic-regression/main.py
+++ tutorials/basics/logistic-regression/main.py
@@ -10,3 +10,8 @@ num_classes = 10
 num_epochs = 5
 batch_size = 100
 learning_rate = 0.001
+# MNIST dataset (images and labels)
+train_dataset = torchvision.datasets.MNIST(root='../../data', 
+                                           train=True, 
+                                           transform=transforms.ToTensor(),
+                                           download=True)
