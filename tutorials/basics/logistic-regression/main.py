254559d424cc2cf9de3f8495b7723966a851a732
SamraAzizi <samarraazizi@gmail.com>
2025-07-22 20:17:18 +0500

    basic pytorch

--- tutorials/basics/logistic-regression/main.py
+++ tutorials/basics/logistic-regression/main.py
@@ -15,3 +15,11 @@ train_dataset = torchvision.datasets.MNIST(root='../../data',
                                            train=True, 
                                            transform=transforms.ToTensor(),
                                            download=True)
+test_dataset = torchvision.datasets.MNIST(root='../../data', 
+                                          train=False, 
+                                          transform=transforms.ToTensor())
+
+# Data loader (input pipeline)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                           batch_size=batch_size, 
+                                           shuffle=True)
