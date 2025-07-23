34e73eafc844f0f0f2f10b81938b9bf0dd7fa7f3
SamraAzizi <samarraazizi@gmail.com>
2025-07-23 17:17:34 +0500

    basic pytorch

--- tutorials/basics/python-basics/main.py
+++ tutorials/basics/python-basics/main.py
@@ -9,3 +9,14 @@ import torchvision.transforms as transforms
 x = torch.tensor(1., requires_grad=True)
 w = torch.tensor(2., requires_grad=True)
 b = torch.tensor(3., requires_grad=True)
+
+# Build a computational graph.
+y = w * x + b    # y = 2 * x + 3
+
+# Compute gradients.
+y.backward()
+
+# Print out the gradients.
+print(x.grad)    # x.grad = 2 
+print(w.grad)    # w.grad = 1 
+print(b.grad)    # b.grad = 1 
