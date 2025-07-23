1a89f4467843135e94df3966a77e0296f1982652
SamraAzizi <samarraazizi@gmail.com>
2025-07-23 17:17:18 +0500

    basic pytorch

--- tutorials/basics/python-basics/main.py
+++ tutorials/basics/python-basics/main.py
@@ -3,3 +3,9 @@ import torchvision
 import torch.nn as nn
 import numpy as np
 import torchvision.transforms as transforms
+
+# 1. Basic autograd example 1 
+# Create tensors.
+x = torch.tensor(1., requires_grad=True)
+w = torch.tensor(2., requires_grad=True)
+b = torch.tensor(3., requires_grad=True)
