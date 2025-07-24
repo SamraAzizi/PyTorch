0a0783a306607f1b3c247274220e5644ebf3eb0e
SamraAzizi <samarraazizi@gmail.com>
2025-07-24 21:25:34 +0500

    basic pytorch

--- tutorials/basics/python-basics/main.py
+++ tutorials/basics/python-basics/main.py
@@ -85,3 +85,19 @@ train_dataset = torchvision.datasets.CIFAR10(root='../../data/',
 image, label = train_dataset[0]
 print (image.size())
 print (label)
+
+# Data loader (this provides queues and threads in a very simple way).
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=64, 
+                                           shuffle=True)
+
+# When iteration starts, queue and thread start to load data from files.
+data_iter = iter(train_loader)
+
+# Mini-batch images and labels.
+images, labels = data_iter.next()
+
+# Actual usage of the data loader is as below.
+for images, labels in train_loader:
+    # Training code should be written here.
+    pass
