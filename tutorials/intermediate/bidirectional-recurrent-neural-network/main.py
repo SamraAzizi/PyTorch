6890670dc5c71c901d6f90a018647f15ee6b7d77
SamraAzizi <samarraazizi@gmail.com>
2025-07-25 21:13:57 +0500

    basic pytorch

--- tutorials/intermediate/bidirectional-recurrent-neural-network/main.py
+++ tutorials/intermediate/bidirectional-recurrent-neural-network/main.py
@@ -6,3 +6,19 @@ import torchvision.transforms as transforms
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyper-parameters
+sequence_length = 28
+input_size = 28
+hidden_size = 128
+num_layers = 2
+num_classes = 10
+batch_size = 100
+num_epochs = 2
+learning_rate = 0.003
+
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root='../../data/',
+                                           train=True, 
+                                           transform=transforms.ToTensor(),
+                                           download=True)
