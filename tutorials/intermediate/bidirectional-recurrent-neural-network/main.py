0a91cdffa26223cd67fbfb8ea5c01ac2121a3431
SamraAzizi <samarraazizi@gmail.com>
2025-07-26 20:21:09 +0500

    basic pytorch

--- tutorials/intermediate/bidirectional-recurrent-neural-network/main.py
+++ tutorials/intermediate/bidirectional-recurrent-neural-network/main.py
@@ -22,3 +22,11 @@ train_dataset = torchvision.datasets.MNIST(root='../../data/',
                                            train=True, 
                                            transform=transforms.ToTensor(),
                                            download=True)
+test_dataset = torchvision.datasets.MNIST(root='../../data/',
+                                          train=False, 
+                                          transform=transforms.ToTensor())
+
+# Data loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size, 
+                                           shuffle=True)
