fdb3fa1b6de6f0f0157024b54619aa91b9f9c3e2
SamraAzizi <samarraazizi@gmail.com>
2025-07-26 20:21:54 +0500

    basic pytorch

--- tutorials/intermediate/bidirectional-recurrent-neural-network/main.py
+++ tutorials/intermediate/bidirectional-recurrent-neural-network/main.py
@@ -41,3 +41,10 @@ class BiRNN(nn.Module):
         super(BiRNN, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
+        self.fc = nn.Linear(hidden_size*2, num_classes)  # 2 for bidirection
+    
+    def forward(self, x):
+        # Set initial states
+        h0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).to(device) # 2 for bidirection 
+        c0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size).to(device)
