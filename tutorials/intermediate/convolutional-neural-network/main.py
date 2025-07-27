57d1c2882f016f389bf5c702320f2f8159cb4dbd
SamraAzizi <samarraazizi@gmail.com>
2025-07-27 20:17:05 +0500

    basic pytorch

--- tutorials/intermediate/convolutional-neural-network/main.py
+++ tutorials/intermediate/convolutional-neural-network/main.py
@@ -31,3 +31,12 @@ train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size, 
                                           shuffle=False)
+
+# Convolutional neural network (two convolutional layers)
+class ConvNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
