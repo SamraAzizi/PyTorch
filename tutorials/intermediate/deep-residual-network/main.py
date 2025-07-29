443026e2fd649b7a503f577e8a834e40f14d4f76
SamraAzizi <samarraazizi@gmail.com>
2025-07-29 21:40:12 +0500

    intermediate pytorch deep residual network

--- tutorials/intermediate/deep-residual-network/main.py
+++ tutorials/intermediate/deep-residual-network/main.py
@@ -6,3 +6,16 @@ import torchvision.transforms as transforms
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+# Hyper-parameters
+num_epochs = 80
+batch_size = 100
+learning_rate = 0.001
+
+# Image preprocessing modules
+transform = transforms.Compose([
+    transforms.Pad(4),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32),
+    transforms.ToTensor()])
