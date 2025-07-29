3bee1231960185e8795cfeb3962d6594641a6fc0
SamraAzizi <samarraazizi@gmail.com>
2025-07-29 21:40:48 +0500

    intermediate pytorch deep residual network

--- tutorials/intermediate/deep-residual-network/main.py
+++ tutorials/intermediate/deep-residual-network/main.py
@@ -30,3 +30,11 @@ train_dataset = torchvision.datasets.CIFAR10(root='../../data/',
 test_dataset = torchvision.datasets.CIFAR10(root='../../data/',
                                             train=False, 
                                             transform=transforms.ToTensor())
+# Data loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
