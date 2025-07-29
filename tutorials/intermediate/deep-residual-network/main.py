dd31f48ba25c42e0525cacce7494c2a3d2427d51
SamraAzizi <samarraazizi@gmail.com>
2025-07-29 21:39:52 +0500

    intermediate pytorch deep residual network

---
+++ tutorials/intermediate/deep-residual-network/main.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
