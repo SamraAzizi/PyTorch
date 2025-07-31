5be4d807f90a097ad947b952062a0534b6250068
SamraAzizi <samarraazizi@gmail.com>
2025-07-31 16:37:34 +0500

    intermediate pytorch deep residual network

--- tutorials/intermediate/deep-residual-network/main.py
+++ tutorials/intermediate/deep-residual-network/main.py
@@ -151,4 +151,16 @@ for epoch in range(num_epochs):
     # Decay learning rate
     if (epoch+1) % 20 == 0:
         curr_lr /= 3
-        update_lr(optimizer, curr_lr)
+        
+        # Test the model
+model.eval()
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
