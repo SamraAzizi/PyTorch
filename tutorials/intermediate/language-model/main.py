14e434ddc566df4b0e6423abf8f53b5c4c266efd
SamraAzizi <samarraazizi@gmail.com>
2025-08-01 12:41:05 +0500

    intermediate pytorch language model

--- tutorials/intermediate/language-model/main.py
+++ tutorials/intermediate/language-model/main.py
@@ -33,3 +33,13 @@ class RNNLM(nn.Module):
         self.embed = nn.Embedding(vocab_size, embed_size)
         self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
         self.linear = nn.Linear(hidden_size, vocab_size)
+
+        def forward(self, x, h):
+        # Embed word ids to vectors
+        x = self.embed(x)
+        
+        # Forward propagate LSTM
+        out, (h, c) = self.lstm(x, h)
+        
+        # Reshape output to (batch_size*sequence_length, hidden_size)
+        out = out.reshape(out.size(0)*out.size(1), out.size(2))
