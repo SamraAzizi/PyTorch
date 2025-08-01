fbcce068e88b2a332d00e5135212df340a514fa8
SamraAzizi <samarraazizi@gmail.com>
2025-08-01 12:40:03 +0500

    intermediate pytorch language model

---
+++ tutorials/intermediate/language-model/main.py
@@ -0,0 +1,7 @@
+# Some part of the code was referenced from below.
+# https://github.com/pytorch/examples/tree/master/word_language_model 
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.nn.utils import clip_grad_norm_
+from data_utils import Dictionary, Corpus
