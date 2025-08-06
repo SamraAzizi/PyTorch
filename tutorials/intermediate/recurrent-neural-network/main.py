a28fae70188ee851a17924e1ecd483382a1d1738
SamraAzizi <samarraazizi@gmail.com>
2025-08-06 18:03:04 +0500

    intermediate python RNN

--- tutorials/intermediate/recurrent-neural-network/main.py
+++ tutorials/intermediate/recurrent-neural-network/main.py
@@ -42,3 +42,10 @@ class RNN(nn.Module):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+    
+    def forward(self, x):
+        # Set initial hidden and cell states 
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device) 
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
