63dfa91232401fd9d61ef7dd3ad7eb6fef93dc3a
SamraAzizi <samarraazizi@gmail.com>
2025-08-09 19:32:02 +0500

    basic pytorch autograd

--- tutorials/basics/autograd/main.py
+++ tutorials/basics/autograd/main.py
@@ -9,3 +9,17 @@ y = x + 2
 # y was created as a result of an operation, so it has a grad_fn attribute.
 # grad_fn: references a Function that has created the Tensor
 print(x) # created by the user -> grad_fn is None
+
+print(y)
+print(y.grad_fn)
+
+# Do more operations on y
+z = y * y * 3
+print(z)
+z = z.mean()
+print(z)
+
+# Let's compute the gradients with backpropagation
+# When we finish our computation we can call .backward() and have all the gradients computed automatically.
+# The gradient for this tensor will be accumulated into .grad attribute.
+# It is the partial derivate of the function w.r.t. the tensor
