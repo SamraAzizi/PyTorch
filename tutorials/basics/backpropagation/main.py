0d133bf890a7a8d26b6efbaa71bc4b5c801bf981
SamraAzizi <samarraazizi@gmail.com>
2025-08-10 22:32:34 +0500

    basic pytorch autograd

--- tutorials/basics/backpropagation/main.py
+++ tutorials/basics/backpropagation/main.py
@@ -5,3 +5,12 @@ y = torch.tensor(2.0)
 
 # This is the parameter we want to optimize -> requires_grad=True
 w = torch.tensor(1.0, requires_grad=True)
+
+# forward pass to compute loss
+y_predicted = w * x
+loss = (y_predicted - y)**2
+print(loss)
+
+# backward pass to compute gradient dLoss/dw
+loss.backward()
+print(w.grad)
