8f3822ec9d8d5d862a6a18d8a6e227afba7c0a34
SamraAzizi <samarraazizi@gmail.com>
2025-08-12 18:20:55 +0500

    basic pytorch autograd

--- tutorials/basics/gradient-auto/main.py
+++ tutorials/basics/gradient-auto/main.py
@@ -8,3 +8,13 @@ import torch
 # here : f = 2 * x
 X = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
 Y = torch.tensor([2, 4, 6, 8], dtype=torch.float32)
+
+w = torch.tensor(0.0, dtype=torch.float32, requires_grad=True)
+
+# model output
+def forward(x):
+    return w * x
+
+# loss = MSE
+def loss(y, y_pred):
+    return ((y_pred - y)**2).mean()
