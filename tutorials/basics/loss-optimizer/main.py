de1310187d148bb35062e367bf8d08641bfc404e
SamraAzizi <samarraazizi@gmail.com>
2025-08-13 17:21:41 +0500

    basic pytorch loss and optimizer

---
+++ tutorials/basics/loss-optimizer/main.py
@@ -0,0 +1,14 @@
+# 1) Design model (input, output, forward pass with different layers)
+# 2) Construct loss and optimizer
+# 3) Training loop
+#       - Forward = compute prediction and loss
+#       - Backward = compute gradients
+#       - Update weights
+
+import torch
+import torch.nn as nn
+
+# Linear regression
+# f = w * x 
+
+# here : f = 2 * x
