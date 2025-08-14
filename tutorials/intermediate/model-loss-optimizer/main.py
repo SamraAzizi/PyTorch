51b10382705a133775b8c71ba61a44f9e3072792
SamraAzizi <samarraazizi@gmail.com>
2025-08-14 18:13:57 +0500

    basic pytorch loss and optimizer

--- tutorials/intermediate/model-loss-optimizer/main.py
+++ tutorials/intermediate/model-loss-optimizer/main.py
@@ -10,3 +10,12 @@ import torch.nn as nn
 
 # Linear regression
 # f = w * x 
+# here : f = 2 * x
+
+# 0) Training samples, watch the shape!
+X = torch.tensor([[1], [2], [3], [4]], dtype=torch.float32)
+Y = torch.tensor([[2], [4], [6], [8]], dtype=torch.float32)
+
+n_samples, n_features = X.shape
+print(f'#samples: {n_samples}, #features: {n_features}')
+# 0) create a test sample
