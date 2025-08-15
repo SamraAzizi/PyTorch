edfcbab3ba931e1826d9bb550a95370c4d7d0be0
SamraAzizi <samarraazizi@gmail.com>
2025-08-15 19:35:31 +0500

    intermediate pytorch data loader

--- tutorials/intermediate/dataloader/main.py
+++ tutorials/intermediate/dataloader/main.py
@@ -6,3 +6,16 @@ import math
 
 # gradient computation etc. not efficient for whole data set
 # -> divide dataset into small batches
+
+'''
+# training loop
+for epoch in range(num_epochs):
+    # loop over all batches
+    for i in range(total_batches):
+        batch_x, batch_y = ...
+'''
+
+# epoch = one forward and backward pass of ALL training samples
+# batch_size = number of training samples used in one forward/backward pass
+# number of iterations = number of passes, each pass (forward+backward) using [batch_size] number of sampes
+# e.g : 100 samples, batch_size=20 -> 100/20=5 iterations for 1 epoch
