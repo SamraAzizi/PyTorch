544c67380b2450a42a8f58448bc57231c96112fb
SamraAzizi <samarraazizi@gmail.com>
2025-08-15 17:38:08 +0500

    basic pytorch loss and optimizer

--- tutorials/intermediate/model-loss-optimizer/main.py
+++ tutorials/intermediate/model-loss-optimizer/main.py
@@ -10,6 +10,7 @@ import torch.nn as nn
 
 # Linear regression
 # f = w * x 
+
 # here : f = 2 * x
 
 # 0) Training samples, watch the shape!
@@ -19,7 +20,6 @@ Y = torch.tensor([[2], [4], [6], [8]], dtype=torch.float32)
 n_samples, n_features = X.shape
 print(f'#samples: {n_samples}, #features: {n_features}')
 # 0) create a test sample
-
 X_test = torch.tensor([5], dtype=torch.float32)
 
 # 1) Design Model, the model has to implement the forward pass!
@@ -65,3 +65,12 @@ for epoch in range(n_iters):
 
     # update weights
     optimizer.step()
+
+    # zero the gradients after updating
+    optimizer.zero_grad()
+
+    if epoch % 10 == 0:
+        [w, b] = model.parameters() # unpack parameters
+        print('epoch ', epoch+1, ': w = ', w[0][0].item(), ' loss = ', l)
+
+print(f'Prediction after training: f(5) = {model(X_test).item():.3f}')
