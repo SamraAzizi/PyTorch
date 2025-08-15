fc9bb1fe3ff8dd3e0fd979fa23c29309a23bb779
SamraAzizi <samarraazizi@gmail.com>
2025-08-15 19:35:46 +0500

    intermediate pytorch data loader

--- tutorials/intermediate/dataloader/main.py
+++ tutorials/intermediate/dataloader/main.py
@@ -19,3 +19,16 @@ for epoch in range(num_epochs):
 # batch_size = number of training samples used in one forward/backward pass
 # number of iterations = number of passes, each pass (forward+backward) using [batch_size] number of sampes
 # e.g : 100 samples, batch_size=20 -> 100/20=5 iterations for 1 epoch
+
+
+# --> DataLoader can do the batch computation for us
+
+# Implement a custom Dataset:
+# inherit Dataset
+# implement __init__ , __getitem__ , and __len__
+
+class WineDataset(Dataset):
+
+    def __init__(self):
+        # Initialize data, download, etc.
+        # read with numpy or pandas
