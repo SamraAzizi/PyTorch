bdbc95c00ecded8761d6bf76e62a6cef61a0f997
SamraAzizi <samarraazizi@gmail.com>
2025-08-16 23:23:41 +0500

    intermediate pytorch data loader

--- tutorials/intermediate/dataloader/main.py
+++ tutorials/intermediate/dataloader/main.py
@@ -39,3 +39,11 @@ class WineDataset(Dataset):
         # here the first column is the class label, the rest are the features
         self.x_data = torch.from_numpy(xy[:, 1:]) # size [n_samples, n_features]
         self.y_data = torch.from_numpy(xy[:, [0]]) # size [n_samples, 1]
+
+        # support indexing such that dataset[i] can be used to get i-th sample
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    # we can call len(dataset) to return the size
+    def __len__(self):
+        return self.n_samples
