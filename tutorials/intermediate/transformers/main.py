161f3eed68bb40da000a3405c05ede2069024eaf
SamraAzizi <samarraazizi@gmail.com>
2025-08-17 18:26:30 +0500

    intermediate pytorch transformers

---
+++ tutorials/intermediate/transformers/main.py
@@ -0,0 +1,16 @@
+'''
+Transforms can be applied to PIL images, tensors, ndarrays, or custom data
+during creation of the DataSet
+
+complete list of built-in transforms: 
+https://pytorch.org/docs/stable/torchvision/transforms.html
+
+On Images
+---------
+CenterCrop, Grayscale, Pad, RandomAffine
+RandomCrop, RandomHorizontalFlip, RandomRotation
+Resize, Scale
+
+On Tensors
+----------
+LinearTransformation, Normalize, RandomErasing
