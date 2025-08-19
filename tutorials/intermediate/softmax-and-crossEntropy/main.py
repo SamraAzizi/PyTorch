1e99d90ca77b87f30f687f6faf76e4854843584b
SamraAzizi <samarraazizi@gmail.com>
2025-08-19 16:57:11 +0500

    intermediate pytorch softmax and crossentropy

--- tutorials/intermediate/softmax-and-crossEntropy/main.py
+++ tutorials/intermediate/softmax-and-crossEntropy/main.py
@@ -10,3 +10,13 @@ import numpy as np
 #     scores(logits)      probabilities
 #                           sum = 1.0
 #
+
+# Softmax applies the exponential function to each element, and normalizes
+# by dividing by the sum of all these exponentials
+# -> squashes the output to be between 0 and 1 = probability
+# sum of all probabilities is 1
+def softmax(x):
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+x = np.array([2.0, 1.0, 0.1])
+outputs = softmax(x)
