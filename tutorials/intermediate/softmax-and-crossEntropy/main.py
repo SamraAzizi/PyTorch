102917237c459a0d0c387e849f709693835745f9
SamraAzizi <samarraazizi@gmail.com>
2025-08-20 13:16:41 +0500

    intermediate pytorch softmax and crossentropy

--- tutorials/intermediate/softmax-and-crossEntropy/main.py
+++ tutorials/intermediate/softmax-and-crossEntropy/main.py
@@ -92,4 +92,16 @@ Y = torch.tensor([2, 0, 1])
 Y_pred_good = torch.tensor(
     [[0.1, 0.2, 3.9], # predict class 2
     [1.2, 0.1, 0.3], # predict class 0
-    [0.3, 2.2, 0.2]]) # predict class 1
+
+
+   [0.3, 2.2, 0.2]]) # predict class 1
+
+Y_pred_bad = torch.tensor(
+    [[0.9, 0.2, 0.1],
+    [0.1, 0.3, 1.5],
+    [1.2, 0.2, 0.5]])
+
+l1 = loss(Y_pred_good, Y)
+l2 = loss(Y_pred_bad, Y)
+print(f'Batch Loss1:  {l1.item():.4f}')
+print(f'Batch Loss2: {l2.item():.4f}')
