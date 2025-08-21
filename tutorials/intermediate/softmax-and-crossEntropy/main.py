31d54eb260bd3729a8f445fb9de38e521882d37f
SamraAzizi <samarraazizi@gmail.com>
2025-08-21 22:08:04 +0500

    intermediate pytorch softmax and crossentropy

--- tutorials/intermediate/softmax-and-crossEntropy/main.py
+++ tutorials/intermediate/softmax-and-crossEntropy/main.py
@@ -135,3 +135,6 @@ criterion = nn.BCELoss()
 class NeuralNet2(nn.Module):
     def __init__(self, input_size, hidden_size, num_classes):
         super(NeuralNet2, self).__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size) 
+        self.relu = nn.ReLU()
+        self.linear2 = nn.Linear(hidden_size, num_classes)
