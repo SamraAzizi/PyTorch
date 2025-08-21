923f4cb82257b88578c7c95599d4a56de28f1f74
SamraAzizi <samarraazizi@gmail.com>
2025-08-21 22:08:21 +0500

    intermediate pytorch softmax and crossentropy

--- tutorials/intermediate/softmax-and-crossEntropy/main.py
+++ tutorials/intermediate/softmax-and-crossEntropy/main.py
@@ -138,3 +138,10 @@ class NeuralNet2(nn.Module):
         self.linear1 = nn.Linear(input_size, hidden_size) 
         self.relu = nn.ReLU()
         self.linear2 = nn.Linear(hidden_size, num_classes)
+
+         def forward(self, x):
+        out = self.linear1(x)
+        out = self.relu(out)
+        out = self.linear2(out)
+        # no softmax at the end
+        return out
