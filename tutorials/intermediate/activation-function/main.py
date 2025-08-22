37475540d03da472a007cb7fdd8e28528e0c8e02
SamraAzizi <samarraazizi@gmail.com>
2025-08-22 09:21:58 +0500

    intermediate pytorch softmax and crossentropy

--- tutorials/intermediate/activation-function/main.py
+++ tutorials/intermediate/activation-function/main.py
@@ -7,3 +7,12 @@ import torch.nn.functional as F
 x = torch.tensor([-1.0, 1.0, 2.0, 3.0])
 
 # sofmax
+output = torch.softmax(x, dim=0)
+print(output)
+sm = nn.Softmax(dim=0)
+output = sm(x)
+print(output)
+
+# sigmoid 
+output = torch.sigmoid(x)
+print(output)
