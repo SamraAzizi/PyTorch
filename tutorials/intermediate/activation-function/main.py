e6f93041da49ba38aef6e542269d085d5a867d82
SamraAzizi <samarraazizi@gmail.com>
2025-08-22 09:21:32 +0500

    intermediate pytorch softmax and crossentropy

---
+++ tutorials/intermediate/activation-function/main.py
@@ -0,0 +1,9 @@
+# output = w*x + b
+# output = activation_function(output)
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+x = torch.tensor([-1.0, 1.0, 2.0, 3.0])
+
+# sofmax
