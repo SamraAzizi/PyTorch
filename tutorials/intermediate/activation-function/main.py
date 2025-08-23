bc1800393d9c093bd3470d5f63c534dcc6c82840
SamraAzizi <samarraazizi@gmail.com>
2025-08-23 15:40:30 +0500

    intermediate pytorch activation fucntion

--- tutorials/intermediate/activation-function/main.py
+++ tutorials/intermediate/activation-function/main.py
@@ -64,3 +64,10 @@ class NeuralNet(nn.Module):
         return out
 
 # option 2 (use activation functions directly in forward pass)
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(NeuralNet, self).__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, 1)
+    
