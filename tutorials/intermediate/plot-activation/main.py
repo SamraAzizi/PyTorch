9dd02f671081be8be53c3b2658a126ba333d6ef9
SamraAzizi <samarraazizi@gmail.com>
2025-08-24 18:24:11 +0500

    intermediate pytorch plot activation fucntion

--- tutorials/intermediate/plot-activation/main.py
+++ tutorials/intermediate/plot-activation/main.py
@@ -7,3 +7,14 @@ sigmoid = lambda x: 1 / (1 + np.exp(-x))
 x=np.linspace(-10,10,10)
 
 y=np.linspace(-10,10,100)
+
+fig = plt.figure()
+plt.plot(y,sigmoid(y),'b', label='linspace(-10,10,100)')
+
+plt.grid(linestyle='--')
+
+plt.xlabel('X Axis')
+
+plt.ylabel('Y Axis')
+
+plt.title('Sigmoid Function')
