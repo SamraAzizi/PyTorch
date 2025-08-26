1305f5013d1fbf997fe6289d45000306d417720f
SamraAzizi <samarraazizi@gmail.com>
2025-08-26 17:20:06 +0500

    intermediate pytorch CNN

--- tutorials/intermediate/CNN/main.py
+++ tutorials/intermediate/CNN/main.py
@@ -8,3 +8,12 @@ import numpy as np
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyper-parameters 
+num_epochs = 5
+batch_size = 4
+learning_rate = 0.001
+
+# dataset has PILImage images of range [0, 1]. 
+# We transform them to Tensors of normalized range [-1, 1]
+transform = transforms.Compose(
