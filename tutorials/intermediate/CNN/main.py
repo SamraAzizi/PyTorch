34a293f5f5fa0ccb42b8b3726b5886db7a5f6743
SamraAzizi <samarraazizi@gmail.com>
2025-08-26 17:21:20 +0500

    intermediate pytorch CNN

--- tutorials/intermediate/CNN/main.py
+++ tutorials/intermediate/CNN/main.py
@@ -26,3 +26,9 @@ train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True,
 
 test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                        download=True, transform=transform)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
+                                          shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
+                                         shuffle=False)
