ec3e9e0304fd801078b90b5071273c234be125f7
SamraAzizi <samarraazizi@gmail.com>
2025-08-27 11:51:31 +0500

    intermediate pytorch CNN

--- tutorials/intermediate/CNN/main.py
+++ tutorials/intermediate/CNN/main.py
@@ -58,3 +58,13 @@ class ConvNet(nn.Module):
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # -> n, 3, 32, 32
+        x = self.pool(F.relu(self.conv1(x)))  # -> n, 6, 14, 14
+        x = self.pool(F.relu(self.conv2(x)))  # -> n, 16, 5, 5
+        x = x.view(-1, 16 * 5 * 5)            # -> n, 400
+        x = F.relu(self.fc1(x))               # -> n, 120
+        x = F.relu(self.fc2(x))               # -> n, 84
+        x = self.fc3(x)                       # -> n, 10
+        return x
