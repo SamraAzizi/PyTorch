81236b925de6c8ddfe9e0f44fe74138fe5e32061
SamraAzizi <samarraazizi@gmail.com>
2025-08-27 11:52:11 +0500

    intermediate pytorch CNN

--- tutorials/intermediate/CNN/main.py
+++ tutorials/intermediate/CNN/main.py
@@ -76,3 +76,10 @@ criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 n_total_steps = len(train_loader)
+
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # origin shape: [4, 3, 32, 32] = 4, 3, 1024
+        # input_layer: 3 input channels, 6 output channels, 5 kernel size
+        images = images.to(device)
+        labels = labels.to(device)
