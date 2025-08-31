28d6e270e614b4e24bd057cfd43251a6a881b4f7
SamraAzizi <samarraazizi@gmail.com>
2025-08-31 16:47:26 +0500

    intermediate pytorch transfer learning

--- tutorials/intermediate/transfer-learning/main.py
+++ tutorials/intermediate/transfer-learning/main.py
@@ -81,3 +81,11 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 
             running_loss = 0.0
             running_corrects = 0
+
+             # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # forward
+                # track history if only in train
