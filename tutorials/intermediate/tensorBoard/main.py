39cd8357b6366b573cefed786668f7559a001be4
SamraAzizi <samarraazizi@gmail.com>
2025-09-02 17:50:30 +0500

    intermediate pytorch transor board

---
+++ tutorials/intermediate/tensorBoard/main.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
+############## TENSORBOARD ########################
+import sys
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
