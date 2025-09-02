a23ce0a922bdbb850577856ce8b3850c2c996b81
SamraAzizi <samarraazizi@gmail.com>
2025-09-02 17:50:56 +0500

    intermediate pytorch transor board

--- tutorials/intermediate/tensorBoard/main.py
+++ tutorials/intermediate/tensorBoard/main.py
@@ -8,3 +8,10 @@ import matplotlib.pyplot as plt
 import sys
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
+
+# default `log_dir` is "runs" - we'll be more specific here
+writer = SummaryWriter('runs/mnist1')
+###################################################
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
