9ec741b04bdbba62aaed6a0bae00581a057044af
SamraAzizi <samarraazizi@gmail.com>
2025-09-04 18:24:46 +0500

    intermediate pytorch transor board

--- tutorials/intermediate/tensorBoard/main.py
+++ tutorials/intermediate/tensorBoard/main.py
@@ -117,3 +117,13 @@ for epoch in range(num_epochs):
             print (f'Epoch [{epoch+1}/{num_epochs}], Step [{i+1}/{n_total_steps}], Loss: {loss.item():.4f}')
             ############## TENSORBOARD ########################
             writer.add_scalar('training loss', running_loss / 100, epoch * n_total_steps + i)
+
+
+            running_accuracy = running_correct / 100 / predicted.size(0)
+            writer.add_scalar('accuracy', running_accuracy, epoch * n_total_steps + i)
+            running_correct = 0
+            running_loss = 0.0
+            ###################################################
+
+# Test the model
+# In test phase, we don't need to compute gradients (for memory efficiency)
